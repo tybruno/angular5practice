fa6e4e5a79eedc42216b2588086391463bdc9abe
Tyler <tyler.bruno@sjsu.edu>
2018-02-11 11:32:26 -0800

    Created data.service

---
+++ src/app/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+
+//this must be imported in app.module to use it.
+@Injectable()
+export class DataService {
+
+  private goals = new BehaviorSubject<any>(['The initial goal', 'Another silly life goal']);
+  goal = this.goals.asObservable();
+
+  constructor() { }
+
+  // this method can be accessed in any component
+  changeGoal(goal) {
+    this.goals.next(goal);
+  }
+}
